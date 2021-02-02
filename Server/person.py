9a682a59cb77d4bff3336630d8b6bbbde8204982
troutking <markarsenault1313@gmail.com>
2021-02-02 17:41:03 -0500

    Fixed set_name, and added docstrings

--- Server/person.py
+++ Server/person.py
@@ -1,10 +1,18 @@
 class Person:
+    """
+    Represents a person. name, socket client and IP
+    """
     def __init__(self, addr, client):
         self.addr = addr
         self.client = client
         self.name = None
 
-    def __set_name__(self, name):
+    def set_name(self, name):
+        """
+        Set a persons name
+        :param name: str
+        :return: None
+        """
         self.name = name
 
     def __repr__(self):
